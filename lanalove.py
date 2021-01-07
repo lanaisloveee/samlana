6e9d5eac27892b00a2257bbdc7e377fb34c348da
lanaisloveee <77107569+lanaisloveee@users.noreply.github.com>
2021-01-07 18:28:47 +0530

    Create lanalove.py

---
+++ lanalove.py
@@ -0,0 +1,58 @@
+#                                    lanapp.py                                           Modified
+#Hey you fuckers
+
+print('welcome to lana is loveeee\n')
+
+print('this is  a python file \n why python? because we smallpp highschool nerds will use python onl                 >
+def checkforaukat(pplen,girl,name):
+        if (name=="lana" or name=="lana rhodes"):
+                print("you don't deserve her")
+        else:
+                if(pplen==1 and girl==1):
+                        print("your dick deserve her")
+                        exit()
+                elif(pplen==2 and girl==1):
+                        print("you don't deserve her fuck off")
+                        exit()
+                elif(pplen==3 and girl==1):
+                        print("how can you even think bruh")
+                        exit()
+                else:
+                        print("fuck off idoit")
+                        exit()
+                if(pplen==1 and girl==2):
+                        print("ofcourse your dick deserve her")
+                        exit()
+                elif(pplen==2 and girl==2):
+                        print("you  deserve her but you cannot get laid with her you fucker")
+                        exit()
+                elif(pplen==3 and girl==2):
+                        print("you can try but don't")
+                        exit()
+                else:
+                        print("fuck off idoit")
+                        exit()
+
+                if(pplen==1 and girl==3):
+                        print("don't waste your dick on her")
+                        exit()
+                elif(pplen==2 and  girl==3):
+                        print("why mannnnn are you drunk your 5 inch pp with ugly bitch huh....")
+                        exit()
+                elif(pplen==3 and girl==3):
+                        print("live your life with her")
+                        exit()
+                else:
+                        print("fuck off idoit")
+                        exit()
+print('your pp size is:')
+print('1.more than 10 \n 2.less than 10 but more than 5 \n 3. less than 5')
+ppl = input('>')
+print('\nWrite the name of your dream girl whom you cannot get')
+try:
+ name=input("")
+except:
+ name=raw_input()
+print('is she: 1. bootyfull 2. everage 3. uglybitch')
+girl=input('>')
+checkforaukat(ppl,girl,name,)
